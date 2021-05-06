9e8c98c19363bd024f16f2d5a93dff694c25297d
Maurício Szabo <mauricio.szabo@gmail.com>
2021-05-06 14:33:35 -0300

    Adding some more aux files

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies []
+  :dependencies [[metosin/malli "0.5.1"]]
 
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
@@ -12,6 +12,7 @@
   :source-paths ["src" "pathom3/src"]
 
   :repl-options {:init-ns user}
+  :aliases {"watch" ["run" "-m" "user/watch"]}
   :profiles {:dev {:dependencies [[thheller/shadow-cljs "2.12.5"]
                                   [check "0.2.0-SNAPSHOT"]]
                    :source-paths ["dev" "test"]}})
