bde6ceb39069247feee98be554dc1debf93b0117
Maurício Szabo <mauricio.szabo@gmail.com>
2021-05-18 19:37:31 -0300

    Changing ways of running tests

--- test/duck_repled/tests.cljs
+++ test/duck_repled/tests.cljs
@@ -3,13 +3,17 @@
             [duck-repled.repl-test]
             [clojure.test :as test]
             [promesa.core :as p]
-            [duck-repled.repl-helpers :as helpers]))
+            [duck-repled.repl-helpers :as helpers]
+            [duck-repled.repl-protocol :as repl]))
+
+(defmethod test/report [::test/default :begin-test-var] [m]
+  (println "Testing:" (test/testing-vars-str m)))
 
 (defn main [ & args]
   (when (-> args first (= "--test"))
     (defmethod test/report [::test/default :summary] [{:keys [test pass fail error]}]
       (println "Ran" test "tests containing" (+ pass fail error) "assertions.")
-      (println fail "failures," error "errors.")
+      (println pass "passed," fail "failures," error "errors.")
       (if (= 0 fail error)
         (js/process.exit 0)
         (js/process.exit 1)))
@@ -17,12 +21,12 @@
       (test/run-all-tests #"duck-repled.*-test")))
 
   (when (-> args count (>= 2))
-    (p/let [repl (helpers/connect-socket! "localhost"
-                                          (-> args second js/parseInt))]
-      (set! helpers/*global-evaluator* repl)
-      (set! helpers/*kind* (or (some->> args (drop 2) first keyword)
-                               :not-shadow))
-      (test/run-all-tests #"duck-repled.*-test")))
+    (set! helpers/*global-evaluator* #(helpers/connect-socket!
+                                       "localhost"
+                                       (-> args second js/parseInt)))
+    (set! helpers/*kind* (or (some->> args (drop 2) first keyword)
+                             :not-shadow))
+    (test/run-all-tests #"duck-repled.*-test"))
 
   (when (= [] args)
     (prn :loaded)))
